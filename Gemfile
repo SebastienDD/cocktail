aafbb5700d6a1adf6ad4eb53cd0d41372e7f7071
Sebastien DathDelcambe <sebastien.dathdelcambe@gmail.com>
2021-02-18 15:19:13 +0100

    challenge 2 really hard

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'simple_form'
-# Use Redis adapter to run Action Cable in production
+# Use Redis adapter to ru n Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,16 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+require "json"
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_hash = JSON.parse(open(url).read)
+p "Creating the hash..."
+
+ingredients_hash['drinks'].each do |info|
+    ingredient = info['strIngredient1']
+    p Ingredient.create(
+      name: ingredient
+      )
+  end
